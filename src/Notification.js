3801a25a3fa1acefc2ef8e951df33531b31ab2ab
Anthony Legrand Sncf <anthony-legrand@live.fr>
2023-09-29 15:54:01 +0200

    🚧Continue Notification

--- src/Notification.js
+++ src/Notification.js
@@ -1,7 +1,32 @@
-class Notification{
+const fs = require('fs')
+const path = require('path')
 
+const TagsList = [ 'warning', 'neutre', 'debug', 'error', 'sucess' ]
+
+class Notification{
+    constructor(title, message, options){
+        this.title = title
+        this.message = message
+        this.options = setOptions(options)
+    }
+
+    setOptions(options){
+        const { tag, important } = options
+        if(TagsList.includes(tag) && typeof important === 'boolean'){
+            return { tag, important }
+        }
+        throw 'Invalide notification data'
+    }
+
+    write(){
+
+    }
+
+    read(){
+
+    }
 }
 
-module.exports = (title, options) => {
-    return new Graph(title, options)
+module.exports = (title, message, options) => {
+    return new Notification(title, message, options)
 }
