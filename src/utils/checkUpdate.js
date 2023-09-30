e504887432f14cfb946a05315b82c5ea3bb10111
anthonylegrand <anthony-legrand@live.fr>
2023-09-30 17:02:12 +0200

    ⚡️Change check update message

--- src/utils/checkUpdate.js
+++ src/utils/checkUpdate.js
@@ -2,11 +2,11 @@ const fetch = require("node-fetch");
 const pjson = require('./../../package.json');
 const NPM_REGISTRY_URL = `https://registry.npmjs.org/${pjson.name}`;
 
-function displayUpdateMessage() {
+function displayUpdateMessage(json) {
     console.log(`
 ------------------[${pjson.name}]------------------
 You're not using the last version
-Do "npm update ${pjson.name}" to get the new features
+Do "npm install ${pjson.name}@${json['dist-tags']?.latest}" to get the new features
 ------------------------------------------------------
     `);
 }
@@ -20,7 +20,7 @@ fetch(NPM_REGISTRY_URL)
 })
 .then(json => {
     if (pjson.version !== json['dist-tags']?.latest && pjson.version !== json['dist-tags']?.development) {
-        displayUpdateMessage();
+        displayUpdateMessage(json);
     }
 })
 .catch(err => console.error(`[ERROR] Graphsboard: ${err.message}`));
